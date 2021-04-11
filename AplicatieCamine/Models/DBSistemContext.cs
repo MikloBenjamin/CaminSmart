8980e22c373d9536fad24de21d7d827d1699c5c7
Benjamin Miklo <65503317+MikloBenjamin@users.noreply.github.com>
2021-04-11 12:09:49 +0300

    Am adaugat ce am avut in repo-ul celalalt

---
+++ AplicatieCamine/Models/DBSistemContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace AplicatieCamine.Models
+{
+    public partial class DBSistemContext : DbContext
+    {
+        public DBSistemContext()
+        {
+        }
+
+        public DBSistemContext(DbContextOptions<DBSistemContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=tcp:server-db-camine.database.windows.net,1433;Initial Catalog=DBSistem;Persist Security Info=False;User ID=adminUser;Password=admincamine123!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
