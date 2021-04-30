008c9ae6eeb1957193adf466344dc745c7746a92
Benjamin Miklo <65503317+MikloBenjamin@users.noreply.github.com>
2021-04-30 17:03:52 +0300

    Modificarile lui Sarah si Responsiveness Camere

---
+++ Models/Administratori.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace AplicatieCamine.Models
+{
+    public partial class Administratori
+    {
+        public int IdAdmin { get; set; }
+        public int? IdCamin { get; set; }
+        public string Nume { get; set; }
+        public string Adresa { get; set; }
+        public string NrTelefon { get; set; }
+        public string Email { get; set; }
+        public virtual Camine IdCaminNavigation { get; set; }
+    }
+}
